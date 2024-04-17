da6b6e9837bd5da58eabd455ff26bea4b0f4c427
Jack Baylor <jackc.baylor@gmail.com>
2024-04-17 15:10:11 -0400

    todo list and welcoming user

--- Radio_Room/Program.cs
+++ Radio_Room/Program.cs
@@ -6,20 +6,26 @@ using Radio_Room;
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Welcome to the Radio Room");
-Console.Write("Press any key to continue or 'enter' to stop.");    
+Console.Write("Press any key to continue or 'enter' to stop."); 
+
+string userInput = Console.ReadLine();  
 
 if (Console.ReadKey().Key != ConsoleKey.Enter)
 {
     Console.Write("Please enter the name you would like to be called during the game: ");
 
-    // this is where the program would ask for what the player would like for their character,
-    // choosing character type and name to be used,
-    // maybe a second name?
     string userName = Console.ReadLine();
 
-    Console.ReadKey();
-
-    Console.ReadKey();
+    Console.WriteLine($"Welcome, {userName}");
+    /*TODO
+    * this is where the program would ask for what the player would like for their character: 
+    * choosing character type and name to be used, 
+    * maybe a second name?
+    * create a separate console.readline for decision making (y or n, or should it be typing the answer?)
+    * create a single story narrative, with various decisions
+    * look at reference repo
+    */
+    
 } else
 {
     Environment.Exit(0);
