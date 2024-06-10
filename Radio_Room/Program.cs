c0bbe954b98d0c2cceee745a0ce2c77969a23883
Jack Baylor <jackc.baylor@gmail.com>
2024-06-10 11:15:38 -0400

    moving gitignore and removing console app project

--- Radio_Room/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-using Radio_Room;
-
-Console.WriteLine("Hello, World!");
-
-Console.WriteLine("Welcome to the Radio Room");
-Console.Write("Press any key to continue or 'enter' to stop.");
