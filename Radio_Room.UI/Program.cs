7860380fa4ca65dc1fef33c2d4dbafed441c65a6
Jack Baylor <jackc.baylor@gmail.com>
2024-06-11 11:55:49 -0400

    Base MudBlazor UI code

---
+++ Radio_Room.UI/Program.cs
@@ -0,0 +1,29 @@
+using MudBlazor.Services;
+using Radio_Room.UI.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
